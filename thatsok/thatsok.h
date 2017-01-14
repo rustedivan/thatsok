a360d249cf1cb64c8fe49c3d8d87770d083eec3f
Ivan Milles <ivan@wildbrain.se>
2017-01-14 10:23:25 +0700

    Initial commit

---
+++ thatsok/thatsok.h
@@ -0,0 +1,19 @@
+//
+//  thatsok.h
+//  thatsok
+//
+//  Created by Ivan Milles on 2017-01-14.
+//  Copyright © 2017 Wildbrain. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for thatsok.
+FOUNDATION_EXPORT double thatsokVersionNumber;
+
+//! Project version string for thatsok.
+FOUNDATION_EXPORT const unsigned char thatsokVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <thatsok/PublicHeader.h>
+
+
