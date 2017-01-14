0c09ddab79a6acf206ec96665e2a7a204f7b2a6e
Ivan Milles <ivan@wildbrain.se>
2017-01-14 14:30:21 +0700

    Configuration: Setup cross-platform build of the framework. - built manual module map - shared umbrella header (empty) - snake-cased module name - both built frameworks share the same name

---
+++ Source/OKAssert.swift
@@ -0,0 +1,13 @@
+//
+//  OKAssert.swift
+//  thatsok
+//
+//  Created by Ivan Milles on 2017-01-14.
+//  Copyright © 2017 Wildbrain. All rights reserved.
+//
+
+import Foundation
+
+public func okassert() {
+	print ("OK Assert")
+}
--- thatsok/ThatsOK.h
+++ thatsok/ThatsOK.h
@@ -6,14 +6,6 @@
 //  Copyright © 2017 Wildbrain. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
-
-//! Project version number for thatsok.
-FOUNDATION_EXPORT double thatsokVersionNumber;
-
-//! Project version string for thatsok.
-FOUNDATION_EXPORT const unsigned char thatsokVersionString[];
-
 // In this header, you should import all the public headers of your framework using statements like #import <thatsok/PublicHeader.h>
 
 
